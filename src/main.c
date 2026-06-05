fd978391b004199601611944697f87b9c7faf8c5
Sameera Juman <sameerajuman@gmail.com>
2026-06-05 15:47:31 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,18 +6,19 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");     // display the prompt $
+  while(1) {
+    printf("$ ");     // display the prompt $
 
-  // read user input
-  char command_buffer[1024];
-  fgets(command_buffer, sizeof(command_buffer), stdin);
-  command_buffer[strcspn(command_buffer, "\n")] = 0;        // remove newline from command
+    // read user input
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = 0;        // remove newline from command
+    
+    // printf("variable command: %s", command);      // DEBUG
+
+    // print error msg 
+    printf("%s: command not found\n", command);
+  }
   
-  // printf("variable command: %s", command_buffer);      // DEBUG
-
-  // print error msg 
-  printf("%s: command not found\n", command_buffer);
-
   return 0;
 }
