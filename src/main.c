140eb9a08408c489a615876571f3038209c15f2a
Sameera Juman <sameerajuman@gmail.com>
2026-06-07 21:52:56 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -28,16 +29,42 @@ int main(int argc, char *argv[]) {
         char* builtin_cmd[] = {"echo", "exit", "type"};
         int i;
         int length = sizeof(builtin_cmd) / sizeof(builtin_cmd[0]);
-        int found = 0;
+        int foundB = 0;
+        int foundE = 0;
         char* after_type = command + 5;
         for (i = 0; i < length; i++) {
           if (strcmp(after_type, builtin_cmd[i]) == 0) {
             printf("%s is a shell builtin\n", after_type);
-            found = 1;
+            foundB = 1;
+            foundE = 1;
             break;
+          } 
+        }
+        if (!foundE) {      // searching for executables
+          // get PATH env variable
+          char p[1000];
+          strcpy(p, getenv("PATH"));
+          char* token = strtok(p, ":");
+          while (token != NULL) {
+            // check if file with the command name exists
+            char filename[100];
+            strcpy(filename, token);
+            strcat(filename, "/");
+            strcat(filename, after_type);
+            if (access(filename, F_OK) == 0) {
+              // check if file has execute permissions
+              if (access(filename, X_OK) == 0) {
+                foundE = 1;
+                printf("%s is %s\n", after_type, filename);
+                break;
+              }
+              // if file exists BUT lacks execute permissions, continue
+            } 
+            token = strtok(NULL, ":");
           }
         }
-        if (!found) {
+
+        if (!foundB && !foundE) {         // no executable is found in any dir
           printf("%s: not found\n", after_type);
         }
       
